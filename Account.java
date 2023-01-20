1adb821a368ba78fcb53cc06c1039f42779e2e79
11707 <ethan.lubeck@vtsd.com>
2023-01-20 07:56:46 -0500

    ght

---
+++ Account.java
@@ -0,0 +1,15 @@
+public class Account {
+
+    public static void main( String [] args){
+
+        Account user = new Account();
+
+
+
+
+    }
+
+
+
+
+}
--- Login.java
+++ Login.java
@@ -1,43 +1,23 @@
 import java.util.Scanner;
 public class Login {
-    public static void main (String args[]){
-        // User will enter Account Number
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(" Please enter your PIN");
+        System.out.println();
+        int pin = 0;
 
-        Scanner accountNum = new Scanner(System.in);
-        System.out.println("Welcome User. Please enter your account number");
-        int AccountNum = accountNum.nextInt();
-
-        boolean okAccountNum = true;
-                do
-                {
-                    System.out.print(" Enter your 5-digit account number ===>> ");
-                    int pin = AccountNum.enterInt();
-                    okAccountNum= checkAccountNum()
-                }
-
-        // User will enter PIN
-        Scanner pinNum = new Scanner(System.in);
-        System.out.println("Welcome User. Please enter your PIN ");
-        int PinNum = pinNum.nextInt();
-
-
-        // First Name of user
-        String firstName;
-       // Last Name of user
-        String lastName;
-
-
-
-
-
-
+        do
+        {
+            System.out.println(" Enter your PIN ====>");
+            pin = input.nextInt();
+            System.out.println();
+            if (pin != 1234)
+                System.out.println(" Incorrect PIN. Please try again.");
+            System.out.println();
+        }
+        while ( pin != 1234);
+        System.out.println("Welcome");
 
     }
-
-
-
-
-
-
 
 }
