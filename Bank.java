4fea786f4601e6e30de789662a9a64c7418a10fd
11707 <ethan.lubeck@vtsd.com>
2023-01-23 10:03:51 -0500

    ght

---
+++ Bank.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Bank {
+
+  public Bank(int checking, int savings) {
+  }
+
+  public static void main (String args[]){
+
+   Bank user = new Bank( 100,200);
+
+
+
+
+  }
+
+
+
+
+}
--- Interface.java
+++ Interface.java
@@ -1,4 +1,12 @@
 public class Interface {
+    public static void main( String args[]){
 
 
+
+
+
+
+
+
+    }
 }
