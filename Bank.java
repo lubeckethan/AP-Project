c62cd6b0875cf2f521bd77977328eb0fcd8f2b68
11707 <ethan.lubeck@vtsd.com>
2023-01-24 09:15:59 -0500

    ght

--- Bank.java
+++ Bank.java
@@ -6,22 +6,35 @@ public class Bank {
         // Deposit or Withdrawal
       Scanner input = new Scanner(System.in);
       System.out.println( " Would you like to Deposit or Withdrawal Money? Press 1 for Deposit and 2 for Withdrawal");
-      double Balance = 1392.34;
-      Balance = input.nextInt();
+      double Balance = 1392.42;
+      double Response = 0;
+      Response = input.nextInt();
       System.out.println();
-      if(Balance != 1 ){
-          System.out.println ("How much would you like to Withdraw?");
+      if(Response != 1 ){
+          System.out.println (" Your current balance is:" + Balance);
+          System.out.println();
+          System.out.println (" How much would you like to Withdraw?");
       }
       else{
-          System.out.println( "How much would you like to Deposit");
+          System.out.println (" Your current balance is:" + Balance);
+          System.out.println( "How much would you like to Deposit?");
       }
+  }
+  public static void userWithdraw(){
+
+
 
 
 
 
   }
 
+public static void userDeposit(){
 
 
+
+
+
+}
 
 }
--- Main.java
+++ Main.java
@@ -2,9 +2,12 @@ public class Main {
 
 public static void main( String[] args){
 
+    //Login
     Login.userLogin();
-
-
+    //Account
+    Bank.userBank();
+    Bank.userWithdraw();
+    Bank.userDeposit();
 
 }
 
