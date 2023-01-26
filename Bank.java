13459b08c8c798c77ba9bc91ea0ba54ffdc21ce8
11707 <ethan.lubeck@vtsd.com>
2023-01-26 07:58:06 -0500

    ght

--- Bank.java
+++ Bank.java
@@ -10,17 +10,43 @@ public class Bank {
       double Response = 0;
       Response = input.nextInt();
       System.out.println();
+     // Make it so when enter anything besides 1 or 2, get error
       if(Response != 1 ){
           System.out.println (" Your current balance is:" + Balance);
           System.out.println();
           System.out.println (" How much would you like to Withdraw?");
       }
+     if(Response != 2){
+         System.out.println(" Unknown Integer. Please respond with 1 or 2.");
+     }
       else{
           System.out.println (" Your current balance is:" + Balance);
           System.out.println( "How much would you like to Deposit?");
       }
   }
-  public static void userWithdraw(){
+  public static void userWithdraw() {
+      // User inputs number to withdraw and withdraws from Balance
+      Scanner input = new Scanner(System.in);
+      double Withdraw = input.nextDouble();
+      double Balance = 1392.42;
+      // Make sure do while loop works if try to withdraw more than balance. break somewhere
+      while (Withdraw < Balance) ;
+      do {
+          double Account = (Balance - Withdraw);
+          System.out.println(" Your new account balance is:" + Account);
+      }
+      while (Withdraw > Balance);
+      do {
+          System.out.println(" Insffucient Funds. Try again");
+      } while( Withdraw >Balance);
+
+
+  }
+  public static void userDeposit() {
+
+
+  }
+
 
 
 
@@ -28,13 +54,3 @@ public class Bank {
 
 
   }
-
-public static void userDeposit(){
-
-
-
-
-
-}
-
-}
