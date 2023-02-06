3c5ea84f3ec960fb5b0648269b326a017d854890
11707 <ethan.lubeck@vtsd.com>
2023-02-06 08:41:11 -0500

    ght

--- Bank.java
+++ Bank.java
@@ -11,13 +11,10 @@ public class Bank {
         Response = input.nextInt();
         System.out.println();
         // Make it so when enter anything besides 1 or 2, get error
-        if (Response != 1) {
+        if (Response == 2) {
             System.out.println(" Your current balance is:" + Balance);
             System.out.println();
-            System.out.println(" How much would you like to Withdraw?");
-        }
-        if (Response != 2) {
-            System.out.println(" Unknown Integer. Please respond with 1 or 2.");
+            System.out.println(" How much would you like to Withdraw? ===>");
         } else {
             System.out.println(" Your current balance is:" + Balance);
             System.out.println("How much would you like to Deposit?");
@@ -27,35 +24,47 @@ public class Bank {
     public static void userWithdraw() {
         // User inputs number to withdraw and withdraws from Balance
         Scanner input = new Scanner(System.in);
-        double Withdraw = input.nextDouble();
+        double Withdraw;
         double Balance = 1392.42;
         // Make sure do while loop works if try to withdraw more than balance. break somewhere
 
         do {
-            double Account = (Balance - Withdraw);
-            System.out.println(" Your new account balance is:" + Account);
+            Withdraw = input.nextDouble();
+            if (Withdraw > Balance) {
+                System.out.println(" You don't have enough money. Try another amount.");
+            }
+
+        } while (Withdraw > Balance);
+
+        Balance -= Withdraw;
+        System.out.println(" Transaction successful. Your new balance is " + Balance);
+
+    }
+
+    public static void userDeposit() {
+        // User inputs number to withdraw and withdraws from Balance
+        Scanner input = new Scanner(System.in);
+        double Deposit;
+        double Balance = 1392.42;
+        // Make sure do while loop works if try to withdraw more than balance. break somewhere
+
+        do {
+            Deposit = input.nextDouble();
+            if (Deposit > Balance) {
+                System.out.println(" Transaction successful. Your new balance is " + Balance);
+
+            }
+            while (Deposit > Balance) ;
+
+            Balance += Deposit;
+            System.out.println(" Transaction successful. Your new balance is " + Balance);
+
+
         }
-        while (Withdraw < Balance);
-
-        System.out.println(" Insffucient Funds. Try again");
-
-
     }
-  public static void userDeposit() {
-
-
-
-
-
-    }
-
-
 
 
 }
-
-
-
 
 
 
