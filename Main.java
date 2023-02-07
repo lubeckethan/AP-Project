16d58121570ec988464d08edc1103b7fc5079501
11707 <ethan.lubeck@vtsd.com>
2023-02-07 07:33:31 -0500

    ght

--- Main.java
+++ Main.java
@@ -11,6 +11,19 @@ public static void main( String[] args){
 
 }
 
+    boolean run = true;
+        do{
+        // Take the user's choice and process it accordingly
+        System.out.println("What would you like to do?");
+        int choice = input.nextInt();
+        switch(choice){
+            case 1: NoteUtilities.addNote(); break;
+            case 2: NoteUtilities.displayNotes(); break;
 
+            default: System.out.println("Sorry - I couldn't understand that.  Please try again."); break;
+        }
+
+
+    }while(run);
 
 }
