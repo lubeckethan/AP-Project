cc1e1e07126020750020c08b581b16d937f3271b
11707 <ethan.lubeck@vtsd.com>
2023-02-08 09:40:45 -0500

    ght

--- Bank.java
+++ Bank.java
@@ -4,8 +4,6 @@ public class Bank {
 
     public static void userBank() {
         // Deposit or Withdrawal
-        int[] responseList = new int[10000];
-        int lastResponse = 0;
         Scanner input = new Scanner(System.in);
         System.out.println(" Would you like to Deposit or Withdrawal Money? Press 1 for Deposit and 2 for Withdrawal");
         double Balance = 1392.42;
@@ -25,8 +23,6 @@ public class Bank {
 
     public static void userWithdraw() {
         // User inputs number to withdraw and withdraws from Balance
-        int[] responseList = new int[10000];
-        int lastResponse = 0;
         Scanner input = new Scanner(System.in);
         double Withdraw;
         double Balance = 1392.42;
@@ -41,32 +37,24 @@ public class Bank {
         } while (Withdraw > Balance);
 
         Balance -= Withdraw;
-        System.out.println(" Transaction successful. Your new balance is " + Balance);
+        System.out.println(" Transaction successful. Your new balance is " + "$"+ Balance);
 
     }
 
     public static void userDeposit() {
         // User inputs number to withdraw and withdraws from Balance
-        int[] responseList = new int[10000];
-        int lastResponse = 0;
         Scanner input = new Scanner(System.in);
         double Deposit;
         double Balance = 1392.42;
         // Make sure do while loop works if try to withdraw more than balance. break somewhere
 
-        do {
             Deposit = input.nextDouble();
-            if (Deposit > Balance) {
-                System.out.println(" Transaction successful. Your new balance is " + Balance);
-
-            }
-            while (Deposit > Balance) ;
 
             Balance += Deposit;
             System.out.println(" Transaction successful. Your new balance is " + Balance);
 
 
-        } while ( Deposit < Balance);
+
 
     }
 
